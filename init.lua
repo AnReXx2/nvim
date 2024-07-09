60d6f7c1058111c31f799379ffd9dd4a7a5d0624
AnReXx <anrid040@gmail.com>
2024-07-09 23:04:50 +0400

    fix html-language-server

--- init.lua
+++ init.lua
@@ -37,3 +37,8 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+require 'lspconfig'.html.setup {
+  cmd = { "vscode-html-language-server", "--stdio" },
+  filetypes = { "html" }
+}
